ca97c1ed272ddf283423d339bea7ccd6c43ead21
Prativa Sahu <sahuprativa7356@gmail.com>
2026-06-08 17:46:17 +0530

    Update first_repeating.py

--- optimization_practice/first_repeating.py
+++ optimization_practice/first_repeating.py
@@ -83,3 +83,30 @@ element, giving an overall O(n) algorithm.
 =================================================
 
 """
+def first_repeating_brute(nums):
+    
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                return nums[j]  
+
+    return -1 
+
+
+def first_repeating_fast(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return num  
+
+        seen.add(num)
+
+    return -1 
+
+
+
+nums = [10, 5, 3, 4, 3, 5, 6]
+
+print("Brute Force:", first_repeating_brute(nums))
+print("Optimized:  ", first_repeating_fast(nums))
