877709d2931e02c30d859c8fc7b89ddb4a6758ec
Prativa Sahu <sahuprativa7356@gmail.com>
2026-06-08 17:48:46 +0530

    Update two_sum.py

--- optimization_practice/two_sum.py
+++ optimization_practice/two_sum.py
@@ -87,3 +87,36 @@ O(1) time, giving an overall O(n) algorithm.
 =================================================
 
 """
+def two_sum_brute(nums, target):
+   
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return (i, j)
+
+
+def two_sum_fast(nums, target):
+   
+    seen = {}
+
+    for i in range(len(nums)):
+        x = nums[i]
+        complement = target - x
+
+      
+        if complement in seen:
+            return (seen[complement], i)
+
+       
+        seen[x] = i
+
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print("Brute Force:", two_sum_brute(nums, target))
+print("Optimized:  ", two_sum_fast(nums, target))
+
+print("Brute Force Time Complexity: O(n^2)")
+print("Optimized Time Complexity:   O(n)")
